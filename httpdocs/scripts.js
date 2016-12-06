91b7b7f98a7d932bbfd8564bec6f2d6292e5d4bb
Aristeidis Noulis <arisnoul@csd.auth.gr>
2016-12-06 12:45:55 +0200

    Script in original form

--- httpdocs/scripts.js
+++ httpdocs/scripts.js
@@ -1,24 +1,24 @@
 (function() {
   var questions = [{
-    question: "What's the nickname for Michael Peter Balzary?",
-    choices: [Flear, Flea, Fleason, Fleat, Fl],
+    question: "What is 2*5?",
+    choices: [1, 5, 10, 15, 20],
     correctAnswer: 2
   }, {
-    question: "What's the nickname for Michael Peter Balzary?",
-    choices: [Flear, Flea, Fleason, Fleat, Fl],
-    correctAnswer: 2
+    question: "What is 3*6?",
+    choices: [3, 6, 9, 12, 18],
+    correctAnswer: 4
   }, {
-    question: "What's the nickname for Michael Peter Balzary?",
-    choices: [Flear, Flea, Fleason, Fleat, Fl],
-    correctAnswer: 2
+    question: "What is 8*9?",
+    choices: [72, 99, 108, 134, 156],
+    correctAnswer: 0
   }, {
-    question: "What's the nickname for Michael Peter Balzary?",
-    choices: [Flear, Flea, Fleason, Fleat, Fl],
-    correctAnswer: 2
+    question: "What is 1*7?",
+    choices: [4, 5, 6, 7, 8],
+    correctAnswer: 3
   }, {
-    question: "What's the nickname for Michael Peter Balzary?",
-    choices: [Flear, Flea, Fleason, Fleat, Fl],
-    correctAnswer: 2
+    question: "What is 8*8?",
+    choices: [20, 30, 40, 50, 64],
+    correctAnswer: 4
   }];
   
   var questionCounter = 0; //Tracks question number
