70fb0ee7ca5f6814f8ce4c8ddf9adbca4f46aeec
nkarampi <nkarampi@csd.auth.gr>
2016-12-20 20:53:03 +0200

    Quiz js

--- httpdocs/scripts.js
+++ httpdocs/scripts.js
@@ -1,167 +1,160 @@
-(function() {
-  var questions = [{
-    question: "What is 2*5?",
-    choices: [1, 5, 10, 15, 20],
-    correctAnswer: 2
-  }, {
-    question: "What is 3*6?",
-    choices: [3, 6, 9, 12, 18],
-    correctAnswer: 4
-  }, {
-    question: "What is 8*9?",
-    choices: [72, 99, 108, 134, 156],
-    correctAnswer: 0
-  }, {
-    question: "What is 1*7?",
-    choices: [4, 5, 6, 7, 8],
-    correctAnswer: 3
-  }, {
-    question: "What is 8*8?",
-    choices: [20, 30, 40, 50, 64],
-    correctAnswer: 4
-  }];
-  
-  var questionCounter = 0; //Tracks question number
-  var selections = []; //Array containing user choices
-  var quiz = $('#quiz'); //Quiz div object
-  
-  // Display initial question
-  displayNext();
-  
-  // Click handler for the 'next' button
-  $('#next').on('click', function (e) {
-    e.preventDefault();
-    
-    // Suspend click listener during fade animation
-    if(quiz.is(':animated')) {        
-      return false;
+var pos = 0, find = 0, flag=0, movement=false, test, test_status, question,
+ choice, choices, chA, chB, chC,chCor, correct = 0, first_time = true;
+
+var positions = [];
+
+function randomize(){
+	var k = 0;
+	 while (k < 5) {
+         var randomnumber = Math.floor((Math.random() * 6) + 0);
+          if (positions.indexOf(randomnumber) == -1) {
+              positions[k] = randomnumber;
+              k++;
+          }
+     }
+}
+
+var size = 5;
+
+function prepareDisplay(){
+  quiz_status.innerHTML = "";
+   quiz_question.innerHTML = "";
+     images_container.innerHTML = "";
+      buttons_container.innerHTML = "";
+       document.getElementById("warning").setAttribute("style", "visibility:hidden;")
+}
+
+function start(){
+    prepareDisplay();
+  	randomize();
+    quiz_status.innerHTML = "Test Your Knowledge in RHCP";
+	  buttons_container.innerHTML = "<button  class='button button1 centerize' onclick='play()'>Play</button>";
+}
+
+function printResults(){
+		quiz_status.innerHTML = "Test Completed";
+    quiz_status.innerHTML += "<h2>You got "+correct+" of "+size+" questions correct</h2>";
+    buttons_container.innerHTML= "<button class='button button1 ' onclick='play()'>Play Again</button>";
+		pos = 0;
+		correct = 0;
+}
+
+function play(){
+  prepareDisplay();
+	if(pos<size){
+		index = positions[pos];
+    loadDoc();
+	}else{
+		printResults();
+		randomize();
+		return false;
+	}
+}
+
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      showQuestion(this);
     }
-    choose();
-    
-    // If no user selection, progress is stopped
-    if (isNaN(selections[questionCounter])) {
-      alert('Please make a selection!');
-    } else {
-      questionCounter++;
-      displayNext();
+  };
+  xhttp.open("GET", "quizData.xml", true);
+  xhttp.send();
+}
+
+function showQuestion(xml){
+
+  var xmlDoc = xml.responseXML;
+  var i;
+  var x = xmlDoc.getElementsByTagName("QUESTION");
+  question = x[index].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue;
+	chA = x[index].getElementsByTagName("CHOICE_ONE")[0].childNodes[0].nodeValue;
+	chB = x[index].getElementsByTagName("CHOICE_TWO")[0].childNodes[0].nodeValue;
+	chC = x[index].getElementsByTagName("CHOICE_THREE")[0].childNodes[0].nodeValue;
+	chCor = x[index].getElementsByTagName("ANSWER")[0].childNodes[0].nodeValue;
+
+  quiz_status.innerHTML = "Question "+(pos+1)+" of "+size;
+  quiz_question.innerHTML = ""+question;
+  warning.innerHTML = "nothing";
+	images_container.innerHTML = "<input type='image' class='fake_img' class='img_quiz' id='A' onclick='markQuestion(id)' >";
+  $("#A").attr("src",chA);
+  images_container.innerHTML += "<input type='image' class='fake_img' class='img_quiz' id='B' onclick='markQuestion(id)'>";
+  $("#B").attr("src",chB);
+  images_container.innerHTML += "<input type='image' class='fake_img' class='img_quiz' id='C' onclick='markQuestion(id)'>";
+  $("#C").attr("src",chC);
+  buttons_container.innerHTML = "<button class='button button1' onclick='submitAnswer()'>Submit Answer</button>";
+	buttons_container.innerHTML += "<button class='button button1' onclick='move()'>Next Answer</button>";
+
+}
+
+function refreshImageBorder(){
+  document.getElementById('A').style.borderColor = "grey";
+  document.getElementById('B').style.borderColor = "grey";
+  document.getElementById('C').style.borderColor = "grey";
+
+}
+
+function markQuestion(element){
+  choice = element;
+  refreshImageBorder();
+  document.getElementById(choice).style.borderColor = "blue";
+	find=1;
+   if(first_time){
+      	if(choice == chCor){
+      		 flag=1;
+      		correct++;
+      	}
+      	else{
+      		flag=0;
+      	}
+   }
+}
+
+function submitAnswer(){
+
+    if(find==1)
+	   {
+       if(first_time){
+         movement=true;
+         if(flag == 1){
+          document.getElementById(choice).style.borderColor = "green";
+          warning.innerHTML = "Correct!";
+          document.getElementById("warning").setAttribute("style", "visibility:visible;");
+	       }
+	       else{
+          document.getElementById(choice).style.borderColor = "red";
+          warning.innerHTML = "Wrong!";
+          document.getElementById("warning").setAttribute("style", "visibility:visible;");
+         }
+         first_time=false;
+       }else{
+         warning.innerHTML = "You have already submit an Answear!";
+         document.getElementById("warning").setAttribute("style", "visibility:visible;")
+         document.getElementById("question_result").setAttribute("style", "visibility:hidden;")
+       }
+	  }
+	  else{
+        warning.innerHTML = "Please make a selection first!";
+        document.getElementById("warning").setAttribute("style", "visibility:visible;");
     }
-  });
-  
-  // Click handler for the 'prev' button
-  $('#prev').on('click', function (e) {
-    e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    choose();
-    questionCounter--;
-    displayNext();
-  });
-  
-  // Click handler for the 'Start Over' button
-  $('#start').on('click', function (e) {
-    e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-      return false;
-    }
-    questionCounter = 0;
-    selections = [];
-    displayNext();
-    $('#start').hide();
-  });
-  
-  // Animates buttons on hover
-  $('.button').on('mouseenter', function () {
-    $(this).addClass('active');
-  });
-  $('.button').on('mouseleave', function () {
-    $(this).removeClass('active');
-  });
-  
-  // Creates and returns the div that contains the questions and 
-  // the answer selections
-  function createQuestionElement(index) {
-    var qElement = $('<div>', {
-      id: 'question'
-    });
-    
-    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
-    qElement.append(header);
-    
-    var question = $('<p>').append(questions[index].question);
-    qElement.append(question);
-    
-    var radioButtons = createRadios(index);
-    qElement.append(radioButtons);
-    
-    return qElement;
-  }
-  
-  // Creates a list of the answer choices as radio inputs
-  function createRadios(index) {
-    var radioList = $('<ul>');
-    var item;
-    var input = '';
-    for (var i = 0; i < questions[index].choices.length; i++) {
-      item = $('<li>');
-      input = '<input type="radio" name="answer" value=' + i + ' />';
-      input += questions[index].choices[i];
-      item.append(input);
-      radioList.append(item);
-    }
-    return radioList;
-  }
-  
-  // Reads the user selection and pushes the value to an array
-  function choose() {
-    selections[questionCounter] = +$('input[name="answer"]:checked').val();
-  }
-  
-  // Displays next requested element
-  function displayNext() {
-    quiz.fadeOut(function() {
-      $('#question').remove();
-      
-      if(questionCounter < questions.length){
-        var nextQuestion = createQuestionElement(questionCounter);
-        quiz.append(nextQuestion).fadeIn();
-        if (!(isNaN(selections[questionCounter]))) {
-          $('input[value='+selections[questionCounter]+']').prop('checked', true);
-        }
-        
-        // Controls display of 'prev' button
-        if(questionCounter === 1){
-          $('#prev').show();
-        } else if(questionCounter === 0){
-          
-          $('#prev').hide();
-          $('#next').show();
-        }
-      }else {
-        var scoreElem = displayScore();
-        quiz.append(scoreElem).fadeIn();
-        $('#next').hide();
-        $('#prev').hide();
-        $('#start').show();
-      }
-    });
-  }
-  
-  // Computes score and returns a paragraph element to be displayed
-  function displayScore() {
-    var score = $('<p>',{id: 'question'});
-    
-    var numCorrect = 0;
-    for (var i = 0; i < selections.length; i++) {
-      if (selections[i] === questions[i].correctAnswer) {
-        numCorrect++;
-      }
-    }
-    
-    score.append('You got ' + numCorrect + ' questions out of ' +
-                 questions.length + ' right!!!');
-    return score;
-  }
-})();
+
+}
+
+
+function move(){
+	if(movement)
+	{ pos++;
+	  find=0;
+	  flag=0;
+	  movement=false;
+    first_time = true;
+    document.getElementById("warning").setAttribute("style", "visibility:hidden;")
+	  play();
+	}else{
+    warning.innerHTML = "Please submit an answer!";
+    document.getElementById("warning").setAttribute("style", "visibility:visible;");
+	}
+}
+
+window.addEventListener("load", start, false);
