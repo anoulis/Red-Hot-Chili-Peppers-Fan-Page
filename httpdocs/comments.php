cc4b65c3cb8a989a9bc69622e7c95789df38e466
nkarampi <nkarampi@csd.auth.gr>
2016-12-30 13:02:32 +0200

    Sql connection works. Need to
    -Ajax for refresh
    -No spam
    -modify preview on html, with pages(10coms/page)

--- httpdocs/comments.php
+++ httpdocs/comments.php
@@ -1,4 +1,30 @@
 <?php
-
+	$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
+	if ($connect){
+		mysql_select_db("comments_rhcp",$connect);
+		$query2 = "SELECT * FROM comments";
+		$result = mysql_query ($query2);
+		while ($row = mysql_fetch_array($result)){
+		echo $row['EMAIL'] . "<br>" . $row['COMMENT'];
+		}
+	}
+	else	die("Failed to connect: " .mysql_error());
+	
+	if ($_POST){
+		$email= $_POST["email"];
+		$comment= $_POST["comment"];
+		$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
+		
+		if ($connect){
+			mysql_select_db("comments",$connect);
+			$query = "INSERT INTO comments(EMAIL,COMMENT) VALUES (\"" . $email . "\",\"" . $comment . "\")";
+			if (mysql_query($query)){
+				echo "Success.";
+			}
+			else	die( "Failed: ". mysql_error());
+			
+		}
+		else die("Failed to connect: ".mysql_error());
+	}
 include('comments.html');
 ?> 
