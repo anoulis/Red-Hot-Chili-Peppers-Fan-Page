0cb09ebf8f5259a88b0c9b40defeb82b7aae8abc
nkarampi <nkarampi@csd.auth.gr>
2017-01-02 19:59:17 +0200

    Add files via upload

--- httpdocs/comments.php
+++ httpdocs/comments.php
@@ -1,30 +1,35 @@
 <?php
-	$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
+$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
 	if ($connect){
 		mysql_select_db("comments_rhcp",$connect);
+	}
+	else	 die("Failed to connect: " .mysql_error());
+	
+function show(){
 		$query2 = "SELECT * FROM comments";
 		$result = mysql_query ($query2);
 		while ($row = mysql_fetch_array($result)){
-		echo $row['EMAIL'] . "<br>" . $row['COMMENT'];
+		echo $row['EMAIL'] . "<br>" . $row['COMMENT'] . "<br>".  $row['DATE'] . "<br>" ;
 		}
-	}
-	else	die("Failed to connect: " .mysql_error());
-	
+}
+
 	if ($_POST){
 		$email= $_POST["email"];
 		$comment= $_POST["comment"];
 		$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
-		
 		if ($connect){
 			mysql_select_db("comments",$connect);
+                        if(strlen($name) <= '1'){ $name = 'Guest';}
 			$query = "INSERT INTO comments(EMAIL,COMMENT) VALUES (\"" . $email . "\",\"" . $comment . "\")";
 			if (mysql_query($query)){
-				echo "Success.";
+                              header("location: comments.php");
+                               exit;
 			}
-			else	die( "Failed: ". mysql_error());
+			else	 die( "Failed: ". mysql_error());
 			
 		}
 		else die("Failed to connect: ".mysql_error());
 	}
+
 include('comments.html');
 ?> 
