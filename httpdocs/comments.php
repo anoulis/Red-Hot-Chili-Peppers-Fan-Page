0149730de0c264cf49a8320eba7fe6b5e9912860
nkarampi <nkarampi@csd.auth.gr>
2017-01-03 20:11:36 +0200

    Some pagination basics. Need:
    -more anti scripting (i will do it when it's finished)
    -Pagination
    -Maybe styling

--- httpdocs/comments.php
+++ httpdocs/comments.php
@@ -6,11 +6,55 @@ $connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
 	else	 die("Failed to connect: " .mysql_error());
 	
 function show(){
-		$query2 = "SELECT * FROM comments";
-		$result = mysql_query ($query2);
-		while ($row = mysql_fetch_array($result)){
-		echo  strip_tags($row['EMAIL']) . "<br>" .  strip_tags($row['COMMENT']) . "<br>".   strip_tags($row['DATE']) . "<br>" ;
-		}
+                $rec_limit = 10;
+
+         /* Get total number of records */
+         $sql = "SELECT * FROM comments";
+         $retval = mysql_query( $sql);
+         
+         if(! $retval ) {
+            die('Could not get data1: ' . mysql_error());
+         }
+         $row = mysql_fetch_array($retval);
+         $rec_count = $row[0];
+
+/* Doesn't work*/
+         $page= $_GET{'page'};  
+
+         if( $page !=0) {
+            $page = $page + 1;
+            $offset = $rec_limit * $page ;
+         }else {
+            $offset = 0;
+         }
+  
+         $left_rec = $rec_count - ($page * $rec_limit);
+         $sql = "SELECT * FROM comments LIMIT $offset, $rec_limit";
+            
+         $retval = mysql_query( $sql);
+         
+         if(! $retval ) {
+            die('Could not get data2: ' . mysql_error());
+         }
+         
+         while($row = mysql_fetch_array($retval)) {
+            echo "EMAIL :{$row['EMAIL']}  <br> ".
+               "COM : {$row['COMMENT']} <br> ".
+               "DATE : {$row['DATE']} <br> ".
+               "--------------------------------<br>";
+         }
+        
+         if( $page > 0 ) {
+            $last = $page - 2;
+            echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a> |";
+            echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
+         }else if( $page == 0 ) {
+            echo "<a href = \"$_PHP_SELF?page = $page\">Next 10 Records</a>";
+         }else if( $left_rec < $rec_limit ) {
+            $last = $page - 2;
+            echo "<a href = \"$_PHP_SELF?page = $last\">Last 10 Records</a>";
+         }
+           
 }
 
 	if ($_POST){
