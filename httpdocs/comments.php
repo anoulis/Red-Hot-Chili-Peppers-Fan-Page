dac3b428a111b620380025e827efb4ef70202dad
tsik1 <tsikkinis@csd.auth.gr>
2017-01-04 19:15:36 +0200

    Small modification to adjust with the config file

--- httpdocs/comments.php
+++ httpdocs/comments.php
@@ -1,11 +1,5 @@
 <?php
-$connect= mysql_connect("webpagesdb.it.auth.gr:3306","rhcpuser","rhcp123");
-	if ($connect){
-		mysql_select_db("comments_rhcp",$connect);
-	}
-	else{	 
-		die("Failed to connect: " .mysql_error());
-	}
+include('config.php');
 function show(){
          $rec_limit = 10;
 
